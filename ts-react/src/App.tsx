0b389e7ed2f61fea89e5c30fd0be99c4da49fcad
syorito-hatsuki <enderdron3@gmail.com>
2023-09-26 14:29:28 +0300

    Added project base with styled components

---
+++ ts-react/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+export default function App() {
+    return (
+        <div className="container">
+            <textarea className="inputField"/>
+            <div className="spacer"/>
+            <textarea className="inputField"/>
+        </div>
+    )
+}
