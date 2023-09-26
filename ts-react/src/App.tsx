f9f8bf23daa9f129af1997733c72bf0ed8ddb150
syorito-hatsuki <enderdron3@gmail.com>
2023-09-26 15:38:19 +0300

    Added text converting logic

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css"
+import {useState} from "react";
+import {dictionary} from "./dictionary.ts";
 
 export default function App() {
+
+    const [outputText, setOutputText] = useState("")
+
+    function convertText(inputText: string) {
+        dictionary.forEach((value, key) => {
+            inputText = inputText.replaceAll(key, value)
+        })
+        setOutputText(inputText)
+    }
+
     return (
         <div className="container">
-            <textarea className="inputField"/>
+            <textarea className="inputField" onChange={(event) => convertText(event.target.value)}/>
             <div className="spacer"/>
-            <textarea className="inputField"/>
+            <textarea className="inputField" value={outputText}/>
         </div>
     )
 }
