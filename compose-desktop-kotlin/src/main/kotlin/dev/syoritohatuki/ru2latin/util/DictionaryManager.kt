d7ef9c303a684e389f165897cfb749886b8c4e23
syorito-hatsuki <enderdron3@gmail.com>
2024-10-15 00:05:50 +0300

    Added utils for Compose Desktop version

---
+++ compose-desktop-kotlin/src/main/kotlin/dev/syoritohatuki/ru2latin/util/DictionaryManager.kt
@@ -0,0 +1,26 @@
+package dev.syoritohatuki.ru2latin.util
+
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.nio.file.Paths
+
+object DictionaryManager {
+    private val json = Json { encodeDefaults = true; prettyPrint = true; ignoreUnknownKeys = true }
+    private val configDir: File = Paths.get("", "config", "dictionary").toFile()
+    private val configFile = File(configDir, "config.json")
+
+    init {
+        if (!configFile.exists()) {
+            if (!configDir.exists()) configDir.mkdirs()
+            configFile.apply {
+                createNewFile()
+                writeText(json.encodeToString(DictionaryConfig()))
+            }
+        } else configFile.writeText(json.encodeToString(read()))
+    }
+
+    fun read(): DictionaryConfig {
+        return json.decodeFromString(configFile.readText())
+    }
+}
